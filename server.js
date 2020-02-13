b5a292432b62fedb797684ce8d7988ec53339c52
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-02-13 18:37:01 -0500

    first commit after reset

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
+
+const app = express();
+
+// BodyParser Middleware, now included in Express
+app.use(express.json());
+
+// DB config
+const db = config.get('mongoURI');
+
+// Connect to Mongo
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
